6d287dc64af9640e3af4c9b26204f848404516c7
Puneeth G <puneeth019@gmail.com>
2016-07-31 13:51:57 +0530

    Update tibble.R

--- tibble.R
+++ tibble.R
@@ -1,4 +1,4 @@
-#  `tibble` package instead of `dplyr`
+#  `tibble` package to convert `data.frame` into `tibble`
 
 library(tibble) #load `tibble` package
 iris_tibble <- as_data_frame(mtcars) #convert the class of `mtcars` into `tibble`
