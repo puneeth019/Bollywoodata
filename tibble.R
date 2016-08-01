6468a445a9a34a31c5656fc9bc7bf4a0e804bb8d
Puneeth G <puneeth019@gmail.com>
2016-08-01 14:48:23 +0530

    Update tibble.R

--- tibble.R
+++ tibble.R
@@ -1,4 +1,4 @@
 #  `tibble` package to convert `data.frame` into `tibble`
 
 library(tibble) #load `tibble` package
-mtcars_tibble <- as_data_frame(mtcars) #change the class of `mtcars` into `tibble`
+mtcars_tibble <- as_data_frame(x = mtcars) #change the class of `mtcars` into `tibble`
