1278d5f28f95a48120531950105aad5692a96045
Puneeth G <puneeth019@gmail.com>
2016-08-01 14:48:02 +0530

    Update dplyr.R

--- dplyr.R
+++ dplyr.R
@@ -1,4 +1,4 @@
 #  `dplyr` package to convert `data.frame` into `tibble`
 
 library(dplyr) #load `dplyr` package
-mtcars_tibble <- tbl_df(mtcars) #change the class of `mtcars` into `tibble`
+mtcars_dplyr <- tbl_df(data = mtcars) #change the class of `mtcars` into `tibble`
