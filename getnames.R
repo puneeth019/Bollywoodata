1e5170c7f2e43a15896a4a24d59d46a883058964
Puneeth G <puneeth019@gmail.com>
2016-08-17 10:21:29 +0530

    Update getnames.R

--- getnames.R
+++ getnames.R
@@ -6,14 +6,15 @@ names <- as.character(NULL) # Initialise `names` character vector
 
 getnames <- function(x){
   
-  table_temp <- x
-  table_temp_df <- as.data.frame(x = table_temp)
-  names_temp <- unlist(x = names_temp_df[1], use.names = F)
-  names_char_temp <- as.character(x = names_temp)
-  names <- append(x = names, values = names_char_temp)
-  names
+  table_temp <- x # Assign `x` to variable `table_temp`
+  table_temp_df <- as.data.frame(x = table_temp)  # Convert `table_temp` into a `data.frame`
+  names_temp <- unlist(x = table_temp_df[1], use.names = F) # Unlist & extract first column of `table_temp_df`
+  names_char_temp <- as.character(x = names_temp) # Convert the `factor`s in `names_temp` into `character
+  names <- append(x = names, values = names_char_temp)  # Append the character vectors `names` & `names_char_temp` into single vector
+  names # Retun the character vector `names`
   
 }
 
 names <- unlist(x = sapply(X = table[5:14], FUN = getnames), use.names =  F)
+  # Apply fucntion `getnames` on the other `data.frame`s of `table`
 names <- unique(x = names)
