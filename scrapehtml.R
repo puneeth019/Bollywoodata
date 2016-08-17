44d498a4f36d69e99fcb759f0de004197372ab84
Puneeth G <puneeth019@gmail.com>
2016-08-17 10:22:02 +0530

    Update and rename jewels_list.R to scrapehtml.R

--- scrapehtml.R
+++ scrapehtml.R
@@ -16,19 +16,3 @@ main_doc <- htmlTreeParse(file = "List_of_Bollywood_actresses.html", useInternal
 
 table <- readHTMLTable(doc =  main_doc, header = TRUE)
   # Read all the tables in the html file
-
-jewels_list <- as.character(NULL)
-
-getjewelslist <- function(x){
-  
-  table_temp <- x
-  table_temp_df <- as.data.frame(x = table_temp)
-  jewels_list_temp <- unlist(x = table_temp_df[1], use.names = F)
-  jewels_list_char_temp <- as.character(x = jewels_list_temp)
-  jewels_list <- append(x = jewels_list, values = jewels_list_char_temp)
-  jewels_list
-  
-}
-
-jewels_list <- unlist(x = sapply(X = table[5:14], FUN = getjewelslist), use.names =  F)
-jewels_list <- unique(x = jewels_list)
