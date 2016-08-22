724b315f2f63f6c1b4f747a67c7fd9048202a3c5
Puneeth G <puneeth019@gmail.com>
2016-08-22 17:03:00 +0530

    Update madhubala.R

--- madhubala.R
+++ madhubala.R
@@ -2,7 +2,7 @@
 library("rvest") # Load `XML` package
 library("dplyr") # Load `dplyr` pacakge
 
-setwd("C:/Users/lc067/Documents/DA/Projects/Project1/")
+setwd("~/Documents/DA/Projects/Project1/")
 # Set Working directory
 file_url <- "https://en.wikipedia.org/wiki/Madhubala"
 # Assign the wiki url to `file_url`
@@ -20,3 +20,6 @@ table_madhubala <- select(table_madhubala, -Other_notes)
 table_madhubala$Year <- ifelse(table_madhubala$Year %in% c('', "-", " -", "- ", "_"), NA_character_, table_madhubala$Year)
 table_madhubala$Film <- ifelse(table_madhubala$Film %in% c('', "-", " -", "- ", "_"), NA_character_, table_madhubala$Film)
 table_madhubala$Director <- ifelse(table_madhubala$Director %in% c('', "-", " -", "- ", "_"), NA_character_, table_madhubala$Director)
+
+
+write.csv(x =  table_madhubala, file = "madhubala.csv")
