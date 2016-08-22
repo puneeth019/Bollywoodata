55eaad4a0d48b449008dca0ddef54248a69a4630
Puneeth G <puneeth019@gmail.com>
2016-08-22 16:45:15 +0530

    Delete tibble.R

--- tibble.R
+++
@@ -1,4 +0,0 @@
-#  `tibble` package to convert `data.frame` into `tibble`
-
-library(tibble) #load `tibble` package
-mtcars_tibble <- as_data_frame(x = mtcars) #change the class of `mtcars` into `tibble`
