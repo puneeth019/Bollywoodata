62aa3edc8e440f60039623cacec81eda8c78710b
Puneeth G <puneeth019@gmail.com>
2016-08-24 13:19:24 +0530

    Update madhubala.R

--- madhubala.R
+++ madhubala.R
@@ -13,13 +13,19 @@ table_madhubala <- file_url %>%
   html_table(fill = TRUE, trim = TRUE, header = TRUE)
 
 table_madhubala <- table_madhubala[[1]]
+  # Convert `table_madhubala` from `list` into `data.frame`
 
 names(table_madhubala) <- c("Year", "Film", "Director", "Other_notes")
+  # rename columns
 table_madhubala <- select(table_madhubala, -Other_notes)
+  # remove unnecessary columns
 
-table_madhubala$Year <- ifelse(table_madhubala$Year %in% c('', "-", " -", "- ", "_"), NA_character_, table_madhubala$Year)
-table_madhubala$Film <- ifelse(table_madhubala$Film %in% c('', "-", " -", "- ", "_"), NA_character_, table_madhubala$Film)
-table_madhubala$Director <- ifelse(table_madhubala$Director %in% c('', "-", " -", "- ", "_"), NA_character_, table_madhubala$Director)
+# Clean text
+table_madhubala$Year <- ifelse(table_madhubala$Year %in% c(''), NA_character_, table_madhubala$Year)
+table_madhubala$Film <- ifelse(table_madhubala$Film %in% c(''), NA_character_, table_madhubala$Film)
+table_madhubala$Director <- ifelse(table_madhubala$Director %in% c('', "-"), NA_character_, table_madhubala$Director)
 
+table_madhubala$Film <- gsub(pattern = '[(0-9)+ | \\(+ | \\)+ | \'$]', replacement = '', table_madhubala$Film)
+  # Cleaning text using `regular expressions`
 
 write.csv(x =  table_madhubala, file = "madhubala.csv")
