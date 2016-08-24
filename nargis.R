a7438704df3f979243f71d93751bf39604932a17
Puneeth G <puneeth019@gmail.com>
2016-08-25 00:14:07 +0530

    Update nargis.R

--- nargis.R
+++ nargis.R
@@ -30,10 +30,8 @@ text_nargis$Film <- text_nargis$Year
   # copy column-1 data to column-2
 
 # clean text to get 1st column
-text_nargis$Year <- text_nargis$Film
 text_nargis$Year <- gsub(pattern = '.*\\((.*?)\\)', replacement = '\\1', x = text_nargis$Year)
 text_nargis$Year <- gsub(pattern = '[a-z]', replacement = NA_character_, x = text_nargis$Year)
-text_nargis$Year
 
 # clean text to get 2nd column
 text_nargis$Film <- gsub(pattern = '\\((.*)\\)', replacement = '', x = text_nargis$Film)
