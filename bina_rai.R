0765797d8a157d4f9ecd10a941e2d6cdb8eadfa9
Puneeth G <puneeth019@gmail.com>
2016-09-08 13:56:34 +0530

    Update bina_rai.R

--- bina_rai.R
+++ bina_rai.R
@@ -13,7 +13,7 @@ text_binarai <- file_url %>%
   html_nodes(xpath='//*[@id="mw-content-text"]/ul[2]') %>%
   html_text(trim = T)
 
-# text cleaning
+# clean text
 text_binarai <- str_split(string = text_binarai, pattern = "\n")
 
 text_binarai <- text_binarai[[1]]
@@ -29,11 +29,11 @@ names(text_binarai) <- c("Year", "Film")
 text_binarai$Film <- text_binarai$Year
   # copy column-1 data to column-2
 
-# clean text to get 1st column
+# clean text in column-1
 text_binarai$Year <- str_trim(string = text_binarai$Year)
 text_binarai$Year <- substr(x = text_binarai$Year, start = 1, stop = 4)
 
-# clean text to get 2nd column
+# clean text in column-2
 text_binarai$Film <- substring(text = text_binarai$Film, first = 7)
 text_binarai$Film <- gsub(pattern = "[0-9]|\\[|\\]", replacement = "", x = text_binarai$Film)
 text_binarai$Film <- str_trim(string = text_binarai$Film)
