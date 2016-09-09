e53e09508eda8ca37afa2f8baf21c217eb7c788c
Puneeth G <puneeth019@gmail.com>
2016-09-09 14:56:58 +0530

    Update asha_parekh.R

--- asha_parekh.R
+++ asha_parekh.R
@@ -21,21 +21,34 @@ text_ashaparekh <- as.data.frame(text_ashaparekh)
   # convert `text_ashaparekh` `list` into `data.frame`
 
 text_ashaparekh[2] <- NA
-  # Initialize 2nd column of `text.ashaparekh`
+  # Initialize 2nd column of `text_ashaparekh`
 
-names(text_ashaparekh) <- c("Year", "Film")
-  # rename columns of `text.ashaparekh`
+text_ashaparekh[3] <- NA
+# Initialize 3rd column of `text_ashaparekh`
+
+names(text_ashaparekh) <- c("Year", "Film", "Role")
+  # rename columns of `text_ashaparekh`
 
 text_ashaparekh$Film <- text_ashaparekh$Year
   # copy column-1 data to column-2
 
+text_ashaparekh$Role <- text_ashaparekh$Year
+# copy column-1 data to column-2
+
 # clean text in column-1
-aa <- text_ashaparekh$Year
-aa <- str_trim(string = aa)
+text_ashaparekh$Year <- gsub(pattern = ".*([0-9]{4}).*", replacement = "\\1", x = text_ashaparekh$Year)
+text_ashaparekh$Year <- str_trim(string = text_ashaparekh$Year)
 
 # clean text in column-2
 text_ashaparekh$Film <- gsub(pattern = "(.*)\\(.*", replacement = "\\1", x = text_ashaparekh$Film)
 text_ashaparekh$Film <- gsub(pattern = "(.*)\\(.*", replacement = "\\1", x = text_ashaparekh$Film)
 text_ashaparekh$Film <- str_trim(string = text_ashaparekh$Film)
+
+# clean text in column-3
+text_ashaparekh$Role <- gsub(pattern = ".*\\((.*)", replacement = "\\1", x = text_ashaparekh$Role)
+text_ashaparekh$Role <- gsub(pattern = "[0-9]{4,}|\\)|(\\.\\.\\.\\.)", replacement = "", x = text_ashaparekh$Role)
+text_ashaparekh$Role <- str_trim(string = text_ashaparekh$Role)
+text_ashaparekh$Role <- gsub(pattern = "^$", replacement = NA_character_, x = text_ashaparekh$Role)
+text_ashaparekh$Role <- gsub(pattern = '\\"|\\[|\\]|[0-9]*', replacement = "", x = text_ashaparekh$Role)
 
 write.csv(x = text_ashaparekh, file = "asha_parekh.csv")
