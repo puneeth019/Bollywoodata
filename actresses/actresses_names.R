0b4753f577728de7f56c1029ca1d63bd4a73d0e2
Puneeth G <puneeth019@gmail.com>
2016-09-12 12:46:47 +0530

    Update actresses_names.R

--- actresses/actresses_names.R
+++ actresses/actresses_names.R
@@ -4,7 +4,7 @@ library(dplyr)  # Load `dplyr` package
 library(rvest)  # Load `rvest` package
 library(stringr)# Load `stringr` package 
 
-setwd("~/Documents/DA/Projects/Project1/")
+setwd("~/Documents/DA/Projects/Project1/actresses/")
 # Set Working directory
 file_url <- "https://en.wikipedia.org/wiki/List_of_Bollywood_actresses"
 # Assign the wiki url to `file_url`
