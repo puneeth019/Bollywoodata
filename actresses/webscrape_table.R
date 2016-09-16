7c3adc304f376579559df8f94c466923f03c0548
Puneeth G <puneeth019@gmail.com>
2016-09-16 17:55:47 +0530

    Create webscrape_table.R

---
+++ actresses/webscrape_table.R
@@ -0,0 +1,34 @@
+# Sample script to scrape table from webpage
+
+library(rvest)    # Load `rvest` package
+library(dplyr)    # Load `dplyr` pacakge
+library(stringr)  # Load `stringr` package
+
+setwd("~/Documents/DA/Projects/Project1/actresses/")
+# Set Working directory
+file_url <- "https://en.wikipedia.org/wiki/Madhubala"
+# Assign the wiki url to `file_url`
+
+table_madhubala <- file_url %>%
+  read_html() %>%
+  html_nodes(xpath='//*[@id="mw-content-text"]/table[2]') %>%
+  html_table(fill = TRUE, trim = TRUE, header = TRUE)
+
+table_madhubala <- table_madhubala[[1]]
+  # convert `table_madhubala` from `list` into `data.frame`
+
+names(table_madhubala) <- c("Year", "Film", "Director", "Notes")
+  # rename columns
+
+# clean text in column-2
+table_madhubala$Film <- gsub(pattern = "(.*)\\(.*", replacement = "\\1", x = table_madhubala$Film)
+table_madhubala$Film <- str_trim(string = table_madhubala$Film)
+
+# clean text in column-3
+table_madhubala$Director <- str_trim(string = table_madhubala$Director)
+
+# clean text in column-4
+table_madhubala$Notes <- gsub(pattern = "^$", replacement = NA_character_, x = table_madhubala$Notes)
+table_madhubala$Notes <- str_trim(string = table_madhubala$Notes)
+
+write.csv(x =  table_madhubala, file = "madhubala.csv")
