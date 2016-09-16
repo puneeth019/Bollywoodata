deb65907a2d08bb64db7771daaacb0516a5b2e18
Puneeth G <puneeth019@gmail.com>
2016-09-16 17:56:14 +0530

    Create webscrape_text.R

---
+++ actresses/webscrape_text.R
@@ -0,0 +1,30 @@
+# Sample script to scrape text from webpage
+
+library(rvest)    # Load `rvest` package
+library(dplyr)    # Load `dplyr` package
+library(stringr)  # Load `stringr` package
+
+setwd("~/Documents/DA/Projects/Project1/actresses/")
+# Set Working directory
+file_url <- "http://www.imdb.com/name/nm0706787/"
+# Assign the wiki url to `file_url`
+
+Year <- file_url %>%
+  read_html() %>%
+  html_nodes(css = '#filmography :nth-child(2) .filmo-row .year_column') %>%
+  html_text()
+
+Year <- gsub(pattern = "\n|/I", replacement = "", x = Year)
+Year <- str_trim(string = Year)
+
+Film <- file_url %>%
+  read_html() %>%
+  html_nodes(css = '#filmography :nth-child(2) .filmo-row b a') %>%
+  html_text()
+
+Film <- gsub(pattern = "\\.\\.\\.", replacement = "", Film)
+Film <- str_trim(string = Film)
+
+table_aishwarya  <- data.frame(Year, Film, stringsAsFactors = F)
+
+write.csv(x = table_aishwarya, file = "aishwarya.csv")
