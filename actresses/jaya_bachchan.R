0700a967aa102d68aeca3d54d99219edbb56ff4d
Puneeth G <puneeth019@gmail.com>
2016-09-19 14:47:38 +0530

    Create jaya_bachchan.R

---
+++ actresses/jaya_bachchan.R
@@ -0,0 +1,36 @@
+# Sample script to scrape table from webpage
+
+library(rvest)    # Load `rvest` package
+library(dplyr)    # Load `dplyr` pacakge
+library(stringr)  # Load `stringr` package
+
+setwd("~/Documents/DA/Projects/Project1/actresses/")
+# Set Working directory
+file_url <- "https://en.wikipedia.org/wiki/Jaya_Bachchan"
+# Assign the wiki url to `file_url`
+
+table_jayabachchan <- file_url %>%
+  read_html() %>%
+  html_nodes(xpath='//*[@id="mw-content-text"]/table[2]') %>%
+  html_table(fill = TRUE, trim = TRUE, header = TRUE)
+
+table_jayabachchan <- table_jayabachchan[[1]]
+  # convert `table_jayabachchan` from `list` into `data.frame`
+
+names(table_jayabachchan) <- c("Year", "Film", "Role", "Notes")
+  # rename columns
+
+# clean text in column-2
+aa <- table_jayabachchan$Film
+aa <- gsub(pattern = "\\.\\.\\.", replacement = "", x = aa)
+aa <- str_trim(string = aa)
+
+# clean text in column-3
+table_jayabachchan$Role <- gsub(pattern = "^$", replacement = NA_character_, x = table_jayabachchan$Role)
+table_jayabachchan$Role <- str_trim(string = table_jayabachchan$Role)
+
+# clean text in column-4
+table_jayabachchan$Notes <- gsub(pattern = "^$", replacement = NA_character_, x = table_jayabachchan$Notes)
+table_jayabachchan$Notes <- str_trim(string = table_jayabachchan$Notes)
+
+write.csv(x =  table_jayabachchan, file = "jaya_bachchan.csv")
