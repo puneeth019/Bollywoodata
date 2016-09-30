79316375301e5cf9a7daa48f8c5977d1271dc450
Puneeth G <puneeth019@gmail.com>
2016-09-30 10:39:33 +0530

    Update webscrape_text.R

--- actresses/webscrape_text.R
+++ actresses/webscrape_text.R
@@ -6,23 +6,23 @@ library(stringr)  # Load `stringr` package
 
 setwd("~/Documents/DA/Projects/Project1/actresses/")
 # Set Working directory
-file_url <- "http://www.imdb.com/name/nm0706787/"
+file_url <- ''
 # Assign the wiki url to `file_url`
 
 Year <- file_url %>%
   read_html() %>%
-  html_nodes(css = '#filmography :nth-child(2) .filmo-row .year_column') %>%
+  html_nodes(css = '') %>%
   html_text()
 
-Year <- gsub(pattern = "\n|/I", replacement = "", x = Year)
+Year <- gsub(pattern = "^$", replacement = NA_integer_, x = Year)
 Year <- str_trim(string = Year)
 
 Film <- file_url %>%
   read_html() %>%
-  html_nodes(css = '#filmography :nth-child(2) .filmo-row b a') %>%
+  html_nodes(css = '') %>%
   html_text()
 
-Film <- gsub(pattern = "\\.\\.\\.", replacement = "", Film)
+Film <- gsub(pattern = "^$", replacement = NA_character_, x = Film)
 Film <- str_trim(string = Film)
 
 table_aishwarya  <- data.frame(Year, Film, stringsAsFactors = F)
