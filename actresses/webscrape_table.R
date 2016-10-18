9456c3d8ee3828528d7f3686a5052296a9430524
Puneeth G <puneeth019@gmail.com>
2016-10-18 12:21:30 +0530

    Update webscrape_table.R

--- actresses/webscrape_table.R
+++ actresses/webscrape_table.R
@@ -6,36 +6,37 @@ library(stringr)  # Load `stringr` package
 
 setwd("~/Documents/DA/Projects/Project1/actresses/")
 # Set Working directory
-file_url <- ''
+file_url <- 'https://en.wikipedia.org/wiki/Mandakini_(actress)'
 # Assign the wiki url to `file_url`
 
-table_meenakshi <- file_url %>%
+table_mandakini <- file_url %>%
   read_html() %>%
-  html_nodes(xpath='') %>%
+  html_nodes(xpath='//*[@id="mw-content-text"]/table[2]') %>%
   html_table(fill = TRUE, trim = TRUE, header = TRUE)
 
-table_meenakshi <- table_meenakshi[[1]]
-  # convert `table_meenakshi` from `list` into `data.frame`
+table_mandakini <- table_mandakini[[1]]
+  # convert `table_mandakini` from `list` into `data.frame`
 
-names(table_meenakshi) <- c("Year", "Film", "Role", "Notes")
+names(table_mandakini) <- c("Year", "Film", "Role", "Notes")
 # rename columns
 
 # clean text in column-1
-table_meenakshi$Year <- gsub(pattern = "^$", replacement = NA_integer_, x = table_meenakshi$Year)
-table_meenakshi$Year <- str_trim(string = table_meenakshi$Year)
+table_mandakini$Year <- gsub(pattern = "^$", replacement = NA_character_, x = table_mandakini$Year)
+table_mandakini$Year <- str_trim(string = table_mandakini$Year)
 
 # clean text in column-2
-table_meenakshi$Film <- gsub(pattern = "^$", replacement = NA_character_, x = table_meenakshi$Film)
-table_meenakshi$Film <- str_trim(string = table_meenakshi$Film)
+table_mandakini$Film <- gsub(pattern = "^$", replacement = NA_character_, x = table_mandakini$Film)
+table_mandakini$Film <- str_trim(string = table_mandakini$Film)
 
 # clean text in column-3
-table_meenakshi$Role <- gsub(pattern = "^$", replacement = NA_character_, x = table_meenakshi$Role)
-table_meenakshi$Role <- str_trim(string = table_meenakshi$Role)
+table_mandakini$Role <- gsub(pattern = "^$", replacement = NA_character_, x = table_mandakini$Role)
+table_mandakini$Role <- str_trim(string = table_mandakini$Role)
 
 # clean text in column-4
-table_meenakshi$Notes <- gsub(pattern = "^$", replacement = NA_character_, x = table_meenakshi$Notes)
-table_meenakshi$Notes <- str_trim(string = table_meenakshi$Notes)
+table_mandakini$Notes <- gsub(pattern = "^$", replacement = NA_character_, x = table_mandakini$Notes)
+table_mandakini$Notes <- str_trim(string = table_mandakini$Notes)
 
-table_meenakshi <- select(table_meenakshi, Year, Film, Role, Notes)
+table_mandakini <- select(table_mandakini, Year, Film, Role, Notes)
+table_mandakini <- arrange(table_mandakini, Year, Film)
 
-write.csv(x =  table_meenakshi, file = "meenakshi.csv")
+write.csv(x =  table_mandakini, file = "mandakini.csv")
