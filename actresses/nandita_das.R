10ad43e68400bd14ab505e67cec55c4e6ca7abc5
Puneeth G <puneeth019@gmail.com>
2016-10-21 09:10:09 +0530

    Update nandita_das.R

--- actresses/nandita_das.R
+++ actresses/nandita_das.R
@@ -1,45 +1,35 @@
-# Sample script to scrape table from webpage
+# Sample script to scrape text from webpage
 
 library(rvest)    # Load `rvest` package
-library(dplyr)    # Load `dplyr` pacakge
+library(dplyr)    # Load `dplyr` package
 library(stringr)  # Load `stringr` package
 
 setwd("~/Documents/DA/Projects/Project1/actresses/")
 # Set Working directory
-file_url <- 'https://en.wikipedia.org/wiki/Nandita_Das'
+file_url <- 'http://www.imdb.com/name/nm0201903/'
 # Assign the wiki url to `file_url`
 
-table_nanditadas <- file_url %>%
+Year <- file_url %>%
   read_html() %>%
-  html_nodes(xpath='//*[@id="mw-content-text"]/table[2]') %>%
-  html_table(fill = TRUE, trim = TRUE, header = TRUE)
+  html_nodes(css = '.year_column') %>%
+  html_text()
 
-table_nanditadas <- table_nanditadas[[1]]
-  # convert `table_nanditadas` from `list` into `data.frame`
+Year <- gsub(pattern = "\n|/I", replacement = "", x = Year)
+Year <- gsub(pattern = ".*([0-9]{4}).*", replacement = "\\1", x = Year)
+Year <- gsub(pattern = "^$", replacement = NA_integer_, x = Year)
+Year <- str_trim(string = Year)
 
-names(table_nanditadas) <- c("Year", "Film", "Role", "Language", "Notes")
-# rename columns
+Film <- file_url %>%
+  read_html() %>%
+  html_nodes(css = 'b a') %>%
+  html_text()
 
-# clean text in column-1
-table_nanditadas$Year <- gsub(pattern = "^$", replacement = NA_integer_, x = table_nanditadas$Year)
-table_nanditadas$Year <- str_trim(string = table_nanditadas$Year)
+Film <- gsub(pattern = "\\.\\.\\.", replacement = "", Film)
+Film <- gsub(pattern = "^$", replacement = NA_character_, x = Film)
+Film <- str_trim(string = Film)
 
-# clean text in column-2
-table_nanditadas$Film <- gsub(pattern = "^$", replacement = NA_character_, x = table_nanditadas$Film)
-table_nanditadas$Film <- str_trim(string = table_nanditadas$Film)
+table_nanditadas <- data.frame(Year, Film, stringsAsFactors = F)
+table_nanditadas <- table_nanditadas[!duplicated(Film),]
+table_nanditadas <- arrange(table_nanditadas, Year)
 
-# clean text in column-3
-table_nanditadas$Role <- gsub(pattern = "^$", replacement = NA_character_, x = table_nanditadas$Role)
-table_nanditadas$Role <- str_trim(string = table_nanditadas$Role)
-
-# clean text in column-4
-table_nanditadas$Language <- gsub(pattern = "^$", replacement = NA_character_, x = table_nanditadas$Language)
-table_nanditadas$Language <- str_trim(string = table_nanditadas$Language)
-
-# clean text in column-5
-table_nanditadas$Notes <- gsub(pattern = "^$", replacement = NA_character_, x = table_nanditadas$Notes)
-table_nanditadas$Notes <- str_trim(string = table_nanditadas$Notes)
-
-table_nanditadas <- select(table_nanditadas, Year, Film, Role, Notes, Language)
-
-write.csv(x =  table_nanditadas, file = "nandita_das.csv")
+write.csv(x = table_nanditadas, file = "nandita_das.csv")
