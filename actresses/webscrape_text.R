6d6bf7ca41d41e5e9e993dc61f632553e07d9a56
Puneeth G <puneeth019@gmail.com>
2016-10-21 09:13:44 +0530

    Update webscrape_text.R

--- actresses/webscrape_text.R
+++ actresses/webscrape_text.R
@@ -11,21 +11,25 @@ file_url <- ''
 
 Year <- file_url %>%
   read_html() %>%
-  html_nodes(css = '') %>%
+  html_nodes(css = '.year_column') %>%
   html_text()
 
+Year <- gsub(pattern = "\n|/I", replacement = "", x = Year)
+Year <- gsub(pattern = ".*([0-9]{4}).*", replacement = "\\1", x = Year)
 Year <- gsub(pattern = "^$", replacement = NA_integer_, x = Year)
 Year <- str_trim(string = Year)
 
 Film <- file_url %>%
   read_html() %>%
-  html_nodes(css = '') %>%
+  html_nodes(css = 'b a') %>%
   html_text()
 
+Film <- gsub(pattern = "\\.\\.\\.", replacement = "", Film)
 Film <- gsub(pattern = "^$", replacement = NA_character_, x = Film)
 Film <- str_trim(string = Film)
 
 table_aishwarya <- data.frame(Year, Film, stringsAsFactors = F)
+table_aishwarya <- table_aishwarya[!duplicated(Film),]
 table_aishwarya <- arrange(table_aishwarya, Year)
 
-write.csv(x = table_aishwarya, file = "aishwarya.csv")
+write.csv(x = table_aishwarya, file = "nandita_das.csv")
