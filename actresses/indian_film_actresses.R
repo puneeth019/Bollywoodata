00c224c61ae6645ce73807ee5f11a52c8219bed6
Puneeth G <puneeth019@gmail.com>
2016-10-29 17:29:00 +0530

    Add files via upload

---
+++ actresses/indian_film_actresses.R
@@ -0,0 +1,27 @@
+# Scrape Bollywood Actress names from wiki
+library(XML)    # Load `XML` package
+library(dplyr)  # Load `dplyr` package
+library(rvest)  # Load `rvest` package
+library(stringr)# Load `stringr` package
+
+WorkDir <- 'C:/DA/Projects/Bollywoodata/actresses/'
+setwd(dir = WorkDir)
+
+file_url <- 'https://en.wikipedia.org/wiki/List_of_Indian_film_actresses'
+# Assign the wiki url to `file_url`
+
+names <- file_url %>%
+  read_html() %>%
+  html_nodes(css = '#mw-content-text ul:nth-child(1) a:nth-child(1)') %>%
+  html_text(trim = TRUE)
+
+links <- file_url %>%
+  read_html() %>%
+  html_nodes(css = '#mw-content-text ul:nth-child(1) a:nth-child(1)') %>%
+  html_attr(name = 'href') %>% 
+  lapply(FUN = function(x) paste0("https://en.wikipedia.org/", x)) %>%
+  unlist(use.names = F)
+
+actresses <- data.frame(actresses_name = names, http_link = links, row.names = NULL, stringsAsFactors = F)
+
+write.csv(x = actresses, file = 'indian_film_actresses.csv')
