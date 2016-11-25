c3f4109444cbfb481265319c986b00d24898d59f
Puneeth G <puneeth019@gmail.com>
2016-11-25 14:27:25 +0530

    Add files via upload

---
+++ screenwriters/indian_film_screenwriters_male.R
@@ -0,0 +1,27 @@
+# Scrape Indian Film `Screenwriter` names from wiki
+library(rvest)    # Load `rvest` package
+library(dplyr)    # Load `dplyr` package
+library(stringr)  # Load `stringr` package
+
+WorkDir <- "C:/Users/lc067/Documents/DA/projects/Bollywoodata/screenwriters/"
+setwd(dir = WorkDir)
+
+file_url <- "C:/Users/lc067/Documents/DA/projects/Bollywoodata/screenwriters/male.html"
+# Assign the wiki url to `file_url`
+
+names <- file_url %>%
+  read_html() %>%
+  html_nodes(css = '.mw-category-group a') %>%
+  html_text(trim = TRUE) %>%
+  gsub(pattern = "(.*)\\(.*", replacement = "\\1") %>%
+  str_trim()
+
+links <- file_url %>%
+  read_html() %>%
+  html_nodes(css = '.mw-category-group a') %>%
+  html_attr(name = 'href') %>% 
+  unlist(use.names = F)
+
+screenwriters_male <- data.frame(name = names, http_link = links, row.names = NULL, stringsAsFactors = F)
+
+write.csv(x = screenwriters_male, file = 'indian_film_screenwriters_male.csv')
