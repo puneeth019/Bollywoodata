8a0de72d2905a13a4e8b424d335f84477c7b5817
Puneeth G <puneeth019@gmail.com>
2016-11-25 11:29:53 +0530

    Add files via upload

---
+++ cinematographers/indian_film_cinematographers.R
@@ -0,0 +1,25 @@
+# Scrape Indian Film `Cinematographer` names from wiki
+library(rvest)  # Load `rvest` package
+library(dplyr)  # Load `dplyr` package
+
+WorkDir <- "C:/Users/lc067/Documents/DA/projects/Bollywoodata/cinematographers/"
+setwd(dir = WorkDir)
+
+file_url <- "C:/Users/lc067/Documents/DA/projects/Bollywoodata/cinematographers/aa.html"
+# Assign the wiki url to `file_url`
+
+names <- file_url %>%
+  read_html() %>%
+  html_nodes(css = '.column-count-3 a:nth-child(1)') %>%
+  html_text(trim = TRUE)
+
+links <- file_url %>%
+  read_html() %>%
+  html_nodes(css = '.column-count-3 a:nth-child(1)') %>%
+  html_attr(name = 'href') %>% 
+  unlist(use.names = F)
+
+cinematographers <- data.frame(name = names, http_link = links, row.names = NULL, stringsAsFactors = F) %>%
+  filter(name != "[1]")
+
+write.csv(x = cinematographers, file = 'indian_film_cinematographers.csv')
